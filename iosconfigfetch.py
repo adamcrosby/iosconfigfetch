0748718c66a8c410c39473d93bfb937a7de4206b
Adam Crosby <adam@uptill3.com>
2011-04-17 22:42:53 -0400

    Updated Licensing - EXScript requires GPL2 compat

--- iosconfigfetch.py
+++ iosconfigfetch.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python
 #
-# (c) 2011 Adam Crosby
-# Licensed under: http://creativecommons.org/licenses/by-nc-sa/3.0/
+# Copyright (c) 2011 Adam Crosby
 #
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
 import sys
 from optparse import OptionParser
 from optparse import OptionGroup
